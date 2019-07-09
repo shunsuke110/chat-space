eb1e86d0bcce722ace214007b9fe949c963dac43
shunsuke110 <lasvegasuver110@gmail.com>
2019-07-09 20:43:16 +0900

    edit_model

--- db/schema.rb
+++ db/schema.rb
@@ -20,25 +20,4 @@ ActiveRecord::Schema.define(version: 20190708110959) do
     t.datetime "updated_at",               null: false
   end
 
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "group_id"
-    t.text     "body",       limit: 65535
-    t.string   "image"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
 end
