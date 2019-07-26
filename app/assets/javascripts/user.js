56c48c265b67f643dfa79c77a10675671dc59352
shunsuke110 <lasvegasuver110@gmail.com>
2019-07-26 15:45:49 +0900

    fix

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,7 +4,7 @@ $(function() {
   var select_list = $("#chat-group-users");
 
   function appendUserName(user) {
-    var html = `<div class="chat-group-user clearfix addmember">
+    var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }"  data-user-name="${ user.name }">追加</a>
                 </div>`
@@ -14,8 +14,8 @@ $(function() {
   function selectUserName(user_id, user_name) {
     var remove_html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                           <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
-                          <p class='chat-group-user__name'>${ user_name }</p>
-                          <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                            <p class='chat-group-user__name'>${ user_name }</p>
+                            <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                       </div>`
       select_list.append(remove_html);
     }
