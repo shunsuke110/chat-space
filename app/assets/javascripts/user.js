fbb725d92b012e6959d566e9d2ef8ea1b94fa273
shunsuke110 <lasvegasuver110@gmail.com>
2019-07-26 17:25:10 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -14,8 +14,8 @@ $(function() {
   function selectUserName(user_id, user_name) {
     var remove_html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                           <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
-                            <p class='chat-group-user__name'>${ user_name }</p>
-                            <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                          <p class='chat-group-user__name'>${ user_name }</p>
+                          <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                       </div>`
       select_list.append(remove_html);
     }
